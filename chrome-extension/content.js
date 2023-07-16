3dfd0436c95dae1ef7f3ba6bed9dfe04d01fea92
christopher-conley <christopher-conley@users.noreply.github.com>
2023-07-16 05:50:12 -0400

    Add info and attributions

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,3 +1,13 @@
+// @name         Let Me Multitask, Bro
+// @author       Christopher Conley
+// @copyright    Copyright (C) 2023 Christopher Conley
+// @license      GPL-2.0-only
+// @version      0.0.1
+// @description  A Chrome extension that allows picture-in-picture mode to be used for videos, even if the website has disabled it.
+//
+// Picture in picture icon by Ria Fitriana from https://thenounproject.com/browse/icons/term/picture-in-picture/ Noun Project (CC BY 3.0)
+//
+
 console.log('LMMB: Let Me Multitask, Bro extension triggered.')
 
 var poller = setInterval(function () {
